40a1096217d59481f6154006e7227993ae3413da
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 21:31:58 +0530

    Modified: checking the end points of twolines

--- linecomparsion.java
+++ linecomparsion.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
-class linecomparsion{
-	public static void main(String[] args)
-	{
-	  System.out.println("Welcome to the linecomparsion program");
-	}
+
+class lineComparison 
+{
+
+public static void main(String[] args) 
+{
+
+System.out.println("Welcome to Line Comparison");
+Scanner sc = new Scanner(System.in);
+		System.out.println("Enter co-ordinate x1 : ");
+		double x1=sc.nextDouble();
+		System.out.println("Enter co-ordinate y1 : ");
+		double y1=sc.nextDouble();
+		System.out.println("Enter co-ordinate x2 : ");
+		double x2=sc.nextDouble();
+		System.out.println("Enter co-ordinate y2 : ");
+		double y2=sc.nextDouble();
+
+		double Lenght = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		System.out.println("Length of Line : "+Lenght) ;
 }
+}
+
+
